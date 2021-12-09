1c8e04bc94300a2c1893abb01557bfb240e6913f
oriyonay <oriyonay12@gmail.com>
2021-12-08 18:07:40 -0600

    fix for heroku

--- app.py
+++ app.py
@@ -16,28 +16,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # probably display the home page here
-
-    # to be run only once to set the table up:
-    # setup(table_name=TABLE_NAME)
-
-    # for now, just return the dummy page:
-    return get_data()
+    attributes = ['a1', 'a2']
+    data = [['c1', 'c2'], ['c3', 'c4']]
+    return render_template('table.html', render_table=False, attributes=attributes, data=data)
 
 @app.route('/search', methods=['POST'])
 def search():
     name = request.form.get('name')
 
     return render_template('book.html', name=name)
-
-'''
-@app.route('/<some_param>')
-def get_data():
-    # example of getting data from the database:
-    # (replace attributes with attribute names and fetch data from actual database)
-    # (also note that we can use <some_param>, which is passed into this function from the url.
-    # we'll probably use this to tailor the query)
-    attributes = ['a1', 'a2']
-    data = [['c1', 'c2'], ['c3', 'c4']]
-    return render_template('table.html', render_table=False, attributes=attributes, data=data)
-'''
